072c459e1559d7c517e2112785cd395142981788
Rafael Tadeu <rafaeltadeusilva@gmail.com>
2025-10-06 09:59:49 -0300

    correcao api

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "163080155"
+    serviceWorkerVersion: "2681112902"
   }
 });
