89f27beccd1e2ed7e7c647e060dda70380bb6cf0
RafaelTadeuSilva <rafaeltadeusilva@gmail.com>
2025-11-26 18:43:25 -0300

    reativacao horario funcionamento

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cb1a4c9027d9cfb77a9b8c1124635dce",
+const RESOURCES = {"flutter_bootstrap.js": "7a5d879155c7730cc8572766251c6e69",
 "version.json": "7aacbb2ebbd9cf38d7043f1ee4bc948a",
 "splash/img/light-2x.png": "a9640d5dbe8ffb769deba2047faefec6",
 "splash/img/dark-4x.png": "ef7f5d823ca227620f74c96cca6bea8d",
@@ -15,7 +15,7 @@ const RESOURCES = {"flutter_bootstrap.js": "cb1a4c9027d9cfb77a9b8c1124635dce",
 "splash/img/light-1x.png": "beea72251d91361d3e4caf40fa62d90f",
 "index.html": "ef298b35296b6b193a396197f7e1d6fd",
 "/": "ef298b35296b6b193a396197f7e1d6fd",
-"main.dart.js": "5800c8c2139c07be1e013b8b9127a0ed",
+"main.dart.js": "4621d8607272446a47f5f3e7552904c9",
 "404.html": "df6fd43d003429d69042ba57798aba40",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
