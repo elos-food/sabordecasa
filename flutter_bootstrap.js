2f7acd9a8a55b71104e2103cabd95cff617e8647
Rafael Tadeu <rafaeltadeusilva@gmail.com>
2025-12-11 10:20:56 -0300

    distrito 3 reais frete

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3487866279"
+    serviceWorkerVersion: "3888308501"
   }
 });
